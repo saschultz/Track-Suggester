354e25bb68e1efe4f79a1054e5c8b6f841cc5c37
Sara Schultz <saschultz95@gmail.com>
2017-02-17 14:51:32 -0800

    add branch to open alert before webpage loads.

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,38 @@
 $(document).ready(function() {
+  var student = prompt("Are you a prospective Epicodus student? (Yes/No)")
+    if (student === "yes") {
+      $("#open").show();
+    }
+    else {
+      alert("First Check Out: https://www.epicodus.com, then come back to us here!");
+    }
+
   $("#formOne").submit(function(event) {
     $("#reveal").fadeIn();
 
     var name = $("input#name").val();
-    var start = $("input#start").val();
-    var design = $("input#design").val();
-    var mobile = $("input#mobile").val();
-    var company = $("input#company").val();
-    // // var tech = $("input#tech").val();
-    //
+    // var start = $("input#start").val();
+    // var design = $("input#design").val();
+    // var mobile = $("input#mobile").val();
+    // var company = $("input#company").val();
+
+      // if (design === .val("yes")) {
+      //   $(".track").show("CSS/Design");
+      // } else if (mobile === .val("mobile device")) {
+      //   $(".track").show("Java/Android");
+      // } else if (company === .val("#new")) {
+      //   $(".track").show("Ruby/Rails");
+      // } else {
+      //   alert("TEST");
+      // }
+
+
+
+
+    // $(".track").text(track); //this will need to add survery result into the reveal text
     $(".name").text(name);
-    // $(".design").text(design);
-    // $(".tech").text(tech);
+    // $(".track").text(design);
     $("#reveal").show();
-
-
-
-
-
     event.preventDefault();
   });
 });
