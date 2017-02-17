a9dc959271cde67941b96701e95a72112d07bb0f
Sara Schultz <saschultz95@gmail.com>
2017-02-17 11:37:57 -0800

    add alert when selecting art/design in dropdown.

--- js/scripts.js
+++ js/scripts.js
@@ -1,24 +1,31 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
+
+  $("select").change(function(submit){
+    if($(this).val() === "yes")
+    { alert("We suggest the CSS/Design track!");
+     }
+
+
+    $("#formOne").submit(function(event) {
+
+    });
+
+
+
+
 
     var name = $("input#name").val();
     var design = $("input#design").val();
-    // var tech = $("input#tech").val();
-
+    // // var tech = $("input#tech").val();
+    //
     $(".name").text(name);
-    $(".design").text(design);
+    // $(".design").text(design);
     // $(".tech").text(tech);
     $("#reveal").show();
 
-    if (".design") {
-      alert("We suggest the CSS/Design track!");
-      // else if {
-      // alert("We do not suggest our CSS/Design track.");
-      // }
-      // else {
-      //
-      // }
-    }
+
+
+
 
     event.preventDefault();
   });
