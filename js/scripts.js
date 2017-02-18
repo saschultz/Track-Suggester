bd5cba5282d462b2ea86d0e312317be5c6ce0f77
Sara Schultz <saschultz95@gmail.com>
2017-02-17 16:14:35 -0800

    fix indentation in html file

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+  debugger;
   var student = prompt("Are you a prospective Epicodus student? (Yes/No)")
     if (student === "yes") {
       $("#open").show();
@@ -7,18 +8,26 @@ $(document).ready(function() {
       alert("First Check Out: https://www.epicodus.com, then come back to us here!");
     }
 
+
+
   $("#formOne").submit(function(event) {
     $("#reveal").fadeIn();
 
     var name = $("input#name").val();
-    // var start = $("input#start").val();
-    // var design = $("input#design").val();
-    // var mobile = $("input#mobile").val();
-    // var company = $("input#company").val();
+    var start = $("input#start").val();
+    var design = $("input#design").val();
+    var reveal = $("#reveal").val();
+    var mobile = $("input#mobile").val();
+    var company = $("input#company").val();
 
-      // if (design === .val("yes")) {
-      //   $(".track").show("CSS/Design");
-      // } else if (mobile === .val("mobile device")) {
+
+    // 
+    // if (design === $function(value("yes")) {
+    //   alert("test");
+    // }
+  // if (design === .val("yes")) {
+  //     $(".track").show("CSS/Design");
+  // } // } else if (mobile === .val("mobile device")) {
       //   $(".track").show("Java/Android");
       // } else if (company === .val("#new")) {
       //   $(".track").show("Ruby/Rails");
@@ -26,12 +35,8 @@ $(document).ready(function() {
       //   alert("TEST");
       // }
 
-
-
-
-    // $(".track").text(track); //this will need to add survery result into the reveal text
     $(".name").text(name);
-    // $(".track").text(design);
+    $(".track").text();
     $("#reveal").show();
     event.preventDefault();
   });
