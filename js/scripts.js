dba7a7a13ffd317d884e5b83477ace54bdebebe6
Sara Schultz <sara@>
2017-02-21 18:38:56 -0800

    fix reveal so CSS/Design inserts with design selection

--- js/scripts.js
+++ js/scripts.js
@@ -15,17 +15,18 @@ $(document).ready(function() {
 
     var name = $("input#name").val();
     var start = $("input#start").val();
-    var design = $("#design").val();
+    var fancy = $("input:radio[name=fancy]:checked").val();
     var mobile = $("input:radio[name=device]:checked").val();
     var company = $("input:radio[name=after]:checked").val();
-    var reveal = $("#reveal").val();
+    // var reveal = $("#reveal").val();
 
-
-      if (design) {
-      $("#track").text("CSS/Design");
-        } else if (mobile === mobile) {
-        $(".track").text("Java/Android");
+      if (fancy === "design") {
+      $(".track").text("CSS/Design");
       }
+      console.log(fancy)
+      //   } else if (mobile === mobile) {
+      //   $(".track").text("Java/Android");
+      // }
       // else if (company === .val("#new")) {
       //   $(".track").show("Ruby/Rails");
       // } else {
