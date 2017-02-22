eb0e6a3c2553b41501e04cb76c6c200407aaace8
Sara Schultz <sara@>
2017-02-21 18:25:25 -0800

    correct indentation in HTML and replace selectbox with radio buttons

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,16 @@
 $(document).ready(function() {
-  // debugger;
-  var student = prompt("Are you a prospective Epicodus student? (Yes/No)")
-    if (student === "yes") {
-      $("#open").show();
-    }
-    else {
-      alert("First Check Out: https://www.epicodus.com, then come back to us here!");
-    }
 
-  $("#formOne").submit(function(event) {
+  // debugger;
+  // var student = prompt("Are you a prospective Epicodus student? (Yes/No)")
+  //   if (student === "yes") {
+  //     $("#open").show();
+  //   }
+  //   else {
+  //     alert("First Check Out: https://www.epicodus.com, then come back to us here!");
+  //   }
+  //
+  $("#formOne").submit(function() {
+      event.preventDefault();
 
 
     var name = $("input#name").val();
@@ -16,22 +18,23 @@ $(document).ready(function() {
     var design = $("#design").val();
     var mobile = $("input:radio[name=device]:checked").val();
     var company = $("input:radio[name=after]:checked").val();
-    // var reveal = $("#reveal").val();
+    var reveal = $("#reveal").val();
 
 
-  // if (design === .val("yes")) {
-  //     $(".track").show("CSS/Design");
-  // } // } else if (mobile === .val("mobile device")) {
-      //   $(".track").show("Java/Android");
-      // } else if (company === .val("#new")) {
+      if (design) {
+      $("#track").text("CSS/Design");
+        } else if (mobile === mobile) {
+        $(".track").text("Java/Android");
+      }
+      // else if (company === .val("#new")) {
       //   $(".track").show("Ruby/Rails");
       // } else {
       //   alert("TEST");
       // }
     $("#reveal").fadeIn();
     $(".name").text(name);
-    $(".track").text();
+    // $(".track").text();
     $("#reveal").show();
-    event.preventDefault();
+
   });
 });
